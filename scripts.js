96d21f39c809c1adcfdc23dea95d8e64f87e5f73
Gustavonvp <gustavonvp@gmail.com>
2022-01-11 01:17:42 -0300

    scripts para carregar body

---
+++ scripts.js
@@ -0,0 +1,218 @@
+const Modal = {
+    open() {
+      document
+        .querySelector('.modal-overlay')
+        .classList
+        .add('active');
+    },
+  
+    close() {
+      document
+        .querySelector('.modal-overlay')
+        .classList
+        .remove('active');
+    }
+  }
+
+// EU preciso somar as entradas
+// depois eu preciso somar as saídas e
+//r remover das entradas o valor das saídas
+// assim, eu terei o total
+
+
+
+const Storage = {
+    get() {
+      return JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
+    },
+  
+    set(transactions) {
+      localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions));
+    }
+  }
+  
+
+  const Utils = {
+    formatAmount(value) {
+      value = Number(value.replace(/\,\./g, "")) * 100;
+      
+      return value;
+    },
+  
+    formatDate(date) {
+      const splittedDate = date.split("-");
+      return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+    },
+  
+    
+    formatCurrency(value) {
+      const signal = Number(value) < 0 ? "-" : "";
+  
+      value = String(value).replace(/\D/g, "");
+  
+      value = Number(value) / 100;
+  
+      value = value.toLocaleString("pt-BR", {
+          style: "currency",
+          currency: "BRL"
+      });
+  
+     return signal + value;
+    }
+  }
+
+
+
+const transactions = [
+{
+    id:1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021'
+},
+
+{
+    id:2,
+    description: 'Website',
+    amount: 50000,
+    date: '23/01/2021'
+},
+
+
+{
+    id:3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021'
+},
+
+
+{
+    id:4,
+    description: 'App',
+    amount: 20000,
+    date: '23/01/2021'
+},
+
+
+]
+const Transaction = {
+    all: Storage.get(),
+  
+    add(transaction) {
+      Transaction.all.push(transaction);
+  
+      App.reload();
+    },
+  
+    remove(index) {
+      Transaction.all.splice(index, 1);
+  
+      App.reload();
+    },
+  
+    incomes() {
+      let income = 0;
+      Transaction.all.forEach(transaction => {
+        if(transaction.amount > 0) {
+          income += transaction.amount;
+        }
+      });
+      return income;
+    },
+  
+    expenses() {
+      let expense = 0;
+      Transaction.all.forEach(transaction => {
+        if(transaction.amount < 0) {
+          expense += transaction.amount;
+        }
+      });
+      return expense;
+    },
+  
+    total() {
+      return Transaction.incomes() + Transaction.expenses();
+    }
+  }
+
+// Substituir os dados do HTML com os dados do JS
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+  
+    // Append new Transaction to their container on html
+    addTransaction(transaction, index) {
+      const tr = document.createElement('tr');
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+      tr.dataset.index = index; 
+  
+      DOM.transactionsContainer.appendChild(tr);
+    },
+  
+    // Create the html for a new transaction
+    innerHTMLTransaction(transaction, index) {
+      const CSSclass = transaction.amount > 0 ? "income" : "expense";
+  
+      const amount = Utils.formatCurrency(transaction.amount);
+  
+      const html = 
+      `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img onclick="Transaction.remove(${index})" src="../assets/minus.svg" alt="Remover transação">
+        </td>
+      `;
+  
+      return html;
+    },
+  
+    // Update values of incomes, expenses and total on html
+    updateBalance() {
+      document
+        .getElementById('incomeDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.incomes());
+      document
+        .getElementById('expenseDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.expenses());
+      document
+        .getElementById('totalDisplay')
+        .innerHTML = Utils.formatCurrency(Transaction.total());
+    },
+  
+    // Clear all transactions on html
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = "";
+    }
+  }
+     
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
+
+
+const App = {
+    init() {
+      Transaction.all.forEach(DOM.addTransaction);
+  
+      DOM.updateBalance();
+  
+      Storage.set(Transaction.all);
+    },
+  
+    reload() {
+      DOM.clearTransactions();
+      App.init();
+    }
+  }
+  
+  App.init();
+  
+
+
+// DOM.updateBalance()
+
+
+
