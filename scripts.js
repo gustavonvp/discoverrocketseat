b104c12acdf53fd19604900c9d670941490b1e46
Gustavonvp <gustavonvp@gmail.com>
2022-01-11 02:56:47 -0300

    script that calculate values in form

--- scripts.js
+++ scripts.js
@@ -188,10 +188,68 @@ const DOM = {
     }
   }
      
-transactions.forEach(function(transaction){
-    DOM.addTransaction(transaction)
-})
+// transactions.forEach(function(transaction){
+//     DOM.addTransaction(transaction)
+// })
 
+
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+  
+    getValues() {
+      return {
+        description: Form.description.value,
+        amount: Form.amount.value,
+        date: Form.date.value
+      }
+    },
+  
+    validateFields() {
+      const { description, amount, date } = Form.getValues();
+      
+      if(description.trim() === "" || 
+         amount.trim() === "" || 
+         date.trim() === "" ) {
+          throw new Error("Por favor, preencha todos os campos");
+      }
+    },
+  
+    formatValues() {
+      let { description, amount, date } = Form.getValues();
+      
+      amount = Utils.formatAmount(amount);
+  
+      date = Utils.formatDate(date);
+  
+      return {
+        description,
+        amount,
+        date
+      };
+    },
+  
+    clearFields() {
+      Form.description.value = "";
+      Form.amount.value = "";
+      Form.date.value = "";
+    },
+  
+    submit(event) {
+      event.preventDefault();
+  
+      try {
+          Form.validateFields();
+          const transaction = Form.formatValues();
+          Transaction.add(transaction);
+          Form.clearFields();
+          Modal.close();
+      } catch (error) {
+          alert(error.message);
+      }
+    },
+  }
 
 const App = {
     init() {
@@ -208,7 +266,7 @@ const App = {
     }
   }
   
-  App.init();
+App.init();
   
 
 
